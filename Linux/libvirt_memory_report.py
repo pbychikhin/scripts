d45110f11f6a87d35d9d0c856ab7f2e5d9920d91
Pavlo Bychikhin <pbychikhin@yahoo.com>
2024-09-11 15:05:27 +0300

    libvirt_memory_report.py - test code

--- Linux/libvirt_memory_report.py
+++ Linux/libvirt_memory_report.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python3
 
-print("Hello")
-print("World")
-print("World")
+from subprocess import run, PIPE
+
+def get_vm_list():
+    return run(["virsh", "list", "--uuid"], check=True, stdout=PIPE, universal_newlines=True).stdout.strip().splitlines()
+
+def get_vm_mem_kb(vm_uuid):
+    for line in run(["virsh", "dominfo", vm_uuid], check=True, stdout=PIPE, universal_newlines=True).stdout.strip().splitlines():
+        line_parts = line.split(":")
+        if line_parts[0] == "Used memory":
+            return float(line_parts[1].strip().split()[0]) * 1.024
+
+def get_vm_pid(vm_uuid):
+    return run(["pgrep", "-f", vm_uuid], check=True, stdout=PIPE, universal_newlines=True).stdout.strip().splitlines()[0]
+
+def get_proc_mem_kb(pid):
+    with open("/proc/{}/status".format(pid)) as proc_status:
+        for line in proc_status:
+            line_parts = line.split(":")
+            if line_parts[0] == "VmRSS":
+                return float(line_parts[1].strip().split()[0])
+
+for vm in get_vm_list():
+    print("{}: (vm_mem) {}, (proc_mem) {}".format(vm, get_vm_mem_kb(vm), get_proc_mem_kb(get_vm_pid(vm))))

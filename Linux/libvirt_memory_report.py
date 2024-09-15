30920b095d611609d713425b4b2338be8a77b459
Pavlo Bychikhin <pbychikhin@yahoo.com>
2024-09-15 17:28:21 +0300

    libvirt_memory_report.py - test code

--- Linux/libvirt_memory_report.py
+++ Linux/libvirt_memory_report.py
@@ -3,34 +3,44 @@
 from subprocess import run, PIPE
 from json import dumps
 from base64 import b64encode
+from sys import stderr
 
 def get_vm_list():
     return run(["virsh", "list", "--uuid"], check=True, stdout=PIPE, universal_newlines=True).stdout.strip().splitlines()
 
 def get_vm_mem_kb(vm_uuid):
-    for line in run(["virsh", "dominfo", vm_uuid], check=True, stdout=PIPE, universal_newlines=True).stdout.strip().splitlines():
-        line_parts = line.split(":")
-        if line_parts[0] == "Used memory":
-            return float(line_parts[1].strip().split()[0]) * 1.024
+    rv = dict()
+    rv_keys = ["actual", "rss"]
+    print("DEBUG, virsh dommemstat {}".format(vm_uuid), file=stderr)
+    for line in run(["virsh", "dommemstat", vm_uuid], check=True, stdout=PIPE, universal_newlines=True).stdout.strip().splitlines():
+        line_parts = line.split()
+        if line_parts[0] in rv_keys:
+            rv[line_parts[0]] = float(line_parts[1].strip().split()[0])
+    print("DEBUG, {}".format(rv), file=stderr)
+    return rv
 
 def get_vm_pid(vm_uuid):
     return run(["pgrep", "-f", vm_uuid], check=True, stdout=PIPE, universal_newlines=True).stdout.strip().splitlines()[0]
 
 def get_proc_mem_kb(pid):
+    rv = dict()
+    rv_keys = ["VmRSS"]
     with open("/proc/{}/status".format(pid)) as proc_status:
         for line in proc_status:
             line_parts = line.split(":")
-            if line_parts[0] == "VmRSS":
-                return float(line_parts[1].strip().split()[0])
+            if line_parts[0] in rv_keys:
+                rv[line_parts[0]] = float(line_parts[1].strip().split()[0])
+    return rv
 
 report = dict()
 for vm in get_vm_list():
     vm_mem = get_vm_mem_kb(vm)
     proc_mem = get_proc_mem_kb(get_vm_pid(vm))
-    ratio = proc_mem / vm_mem
+    ratio = vm_mem["rss"] / vm_mem["actual"]
     report[vm] = {
-        "vm_mem": vm_mem / 1000,
-        "proc_mem": proc_mem / 1000,
+        "actual": vm_mem["actual"] / 1024,
+        "rss": vm_mem["rss"] / 1024,
+        "proc_rss": proc_mem["VmRSS"] / 1024,
         "ratio": ratio
     }
 
--- Linux/libvirt_memory_report_merge.py
+++ Linux/libvirt_memory_report_merge.py
@@ -12,4 +12,4 @@ for k, v in load(stdin).items():
 
 sorted_keys = sorted(list(report.keys()), key=lambda a: report[a]["ratio"], reverse=True)
 for k in sorted_keys:
-    print("{}: (vm_mem) {vm_mem:.2f}, (proc_mem) {proc_mem:.2f}, (ratio) {ratio:.2f}".format(k, **report[k]))
+    print("{}: (actual) {actual:.2f}, (rss) {rss:.2f}, (proc_rss) {proc_rss:.2f},(ratio) {ratio:.2f}".format(k, **report[k]))
